56a3c3af65d48d691761884abd656bad3c6ea8a3
Jason Graffius <jasongraffius@gmail.com>
2017-07-06 18:41:13 -0400

    Adding utility functions
    Mostly deals with conversions to java.util.Properties objects

---
+++ src/kafunc/util.clj
@@ -0,0 +1,36 @@
+(ns kafunc.util
+  (:require [clojure.string :as str])
+  (:import (java.util Properties)))
+
+(defn keyword->property
+  "Convert `:key-words` to `\"key.words\"`"
+  [kw]
+  (str/join (replace {\- \.} (next (str kw)))))
+
+(defn- update-keys*
+  [f args [k v]]
+  [(or (apply f k args) k) v])
+
+(defn update-keys
+  [m f & args]
+  (into (empty m) (map (partial update-keys* f args) m)))
+
+(defn- update-vals*
+  [f args [k v]]
+  [k (or (apply f v args) v)])
+
+(defn update-vals
+  [m f & args]
+  (into (empty m) (map (partial update-vals* f args) m)))
+
+(defn- map->properties*
+  [m]
+  (let [prop (Properties.)]
+    (dorun (map #(.put prop %1 %2) m))))
+
+(defn map->properties
+  [m]
+  (-> m
+      (update-keys #(if (keyword? %) keyword->property %))
+      (update-vals #(if (keyword? %) keyword->property %))
+      (map->properties*)))
