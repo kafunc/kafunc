78db6943d91185c73e84cf88bf38bd80379db8c4
Jason Graffius <jasongraffius@gmail.com>
2017-07-06 00:28:45 -0400

    Added PRecord type

--- src/kafunc/interop.clj
+++ src/kafunc/interop.clj
@@ -1,9 +1,12 @@
 (ns kafunc.interop
   "Namespace for interop with Java/Kafka, to keep core as pure clojure"
   (:import (org.apache.kafka.clients.consumer
-             KafkaConsumer ConsumerRecord)))
+             KafkaConsumer ConsumerRecord)
+           (org.apache.kafka.clients.producer
+             KafkaProducer ProducerRecord)))
 
 (defrecord CRecord [key value partition topic timestamp offset checksum])
+(defrecord PRecord [key value partition topic timestamp])
 
 (defn kafka->crecord
   "Convert a Kafka ConsumerRecord to a Clojure record"
@@ -16,3 +19,13 @@
     (.timestamp record)
     (.offset record)
     (.checksum record)))
+
+(defn kafka->precord
+  "Convert a Kafka ProducerRecord to a Clojure record"
+  [^ProducerRecord record]
+  (->PRecord
+    (.key record)
+    (.value record)
+    (.partition record)
+    (.topic record)
+    (.timestamp record)))
