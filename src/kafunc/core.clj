a6294129b6086eb056ab19483e067a331521f4b9
Jason Graffius <jasongraffius@gmail.com>
2017-07-06 19:00:30 -0400

    Creating some dynamic vars for runtime configuration

--- src/kafunc/core.clj
+++ src/kafunc/core.clj
@@ -1,1 +1,15 @@
-(ns kafunc.core)
+(ns kafunc.core
+  (:require [kafunc.interop :as interop]
+            [kafunc.util :as util]))
+
+(def ^:dynamic *kafka-connect*
+  "Connection string to bootstrap servers. Each entry is of the form
+  address:port, and multiple entries are separated by commas."
+  ;; By default, assume a local Kafka server running on the default port
+  "localhost:9092")
+
+(def ^:dynamic *consumer-config*
+  "The default values to use for consumer configuration."
+  ;; By default, use a byte-array deserializer
+  {:key-deserializer        interop/deserializer
+   :value-deserializer      interop/deserializer})
