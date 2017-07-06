a2590f46f30acfec914aa8ab2ec101657fb7cbf4
Jason Graffius <jasongraffius@gmail.com>
2017-07-06 00:08:01 -0400

    Adding Kafka dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject kafunc "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.apache.kafka/kafka_2.11 "0.10.2.1"]
+                 [org.clojure/clojure "1.8.0"]])
