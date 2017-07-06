c8619e89b8c84dc491950e1b6e45d2ea6bb4ef95
Jason Graffius <jasongraffius@gmail.com>
2017-07-06 00:23:36 -0400

    Adding CRecord type

---
+++ src/kafunc/interop.clj
@@ -0,0 +1,17 @@
+(ns kafunc.interop
+  "Namespace for interop with Java/Kafka, to keep core as pure clojure"
+  (:import (org.apache.kafka.clients.consumer
+             KafkaConsumer ConsumerRecord)))
+
+(defrecord CRecord [key value partition topic offset checksum])
+
+(defn kafka->crecord
+  "Convert a Kafka ConsumerRecord to a Clojure record"
+  [^ConsumerRecord record]
+  (->CRecord
+    (.key record)
+    (.value record)
+    (.partition record)
+    (.topic record)
+    (.offset record)
+    (.checksum record)))
