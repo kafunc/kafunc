f477dfceec20259f11e9e8a4bbaabb32249c4973
Jason Graffius <jasongraffius@gmail.com>
2017-07-06 21:16:25 -0400

    Added various consumption functions

--- src/kafunc/core.clj
+++ src/kafunc/core.clj
@@ -15,8 +15,15 @@
   {:key-deserializer   interop/deserializer
    :value-deserializer interop/deserializer})
 
+(def ^:dynamic *deserializer*
+  "A function which takes an argument of an object, and returns a byte array.
+  The byte array returned must return a similar object when passed to
+  *serializer*. The default function introduces no dependencies, but another
+  makes no guarantees of efficiency."
+  interop/io-deserialize)
+
 (defn make-consumer
-  "Create a KafkaConsumer with the given group.
+  "Create a KafkaConsumer with the given group and/or configuration.
 
   If config is supplied, those values will override *consumer-config*, and
   *consumer-config* overrides the default values created in this function."
@@ -26,3 +33,40 @@
              :group-id          group}
             *consumer-config*
             config))))
+
+(defn next-records
+  "Retrieves a collection of the next available records.
+
+  See consumer->record-seq for details of what a record contains."
+  [consumer & [timeout]]
+  (let [timeout (or timeout Long/MAX_VALUE)]
+    (interop/poll consumer timeout)))
+
+(defn consumer->record-seq
+  "Create an infinite lazy seq which contains records consumed by a consumer.
+
+  The keys in the record are:
+    * :topic      - Topic whence the record was consumed
+    * :partition  - Partition whence the record was consumed
+    * :timestamp  - Timestamp of record
+    * :key        - Key of record
+    * :value      - Value of record
+    * :offset     - Offset whence the record was consumed
+
+  Uses the provided deserializer, or *deserializer* if not specified. This
+  value is bound during the creation of the seq, so the binding does not need
+  to be maintained for the lifetime of the seq."
+  [consumer & [deserializer]]
+  (let [deserialize (or deserializer *deserializer*)]
+    (lazy-cat
+      (-> (next-records consumer)
+          (update :key (fnil deserialize nil))
+          (update :value (fnil deserialize nil)))
+      (consumer->record-seq consumer deserializer))))
+
+(defn consumer->value-seq
+  "Creates an infinite lazy seq which contains values consumed by a consumer.
+
+  Useful if topic/partition/key/offset aren't important."
+  [consumer & [deserializer]]
+  (map :value (consumer->record-seq consumer deserializer)))
