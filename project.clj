574612cdaea737afe6720343861673f6764e881f
Jason Graffius <jasongraffius@gmail.com>
2017-08-05 16:37:01 -0400

    Adding repl options to project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.kafka/kafka_2.11 "0.10.2.1"]
-                 [org.clojure/clojure "1.8.0"]])
+                 [org.clojure/clojure "1.8.0"]]
+  :repl-options {:init-ns kafunc.core})
