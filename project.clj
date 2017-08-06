58fdab29ed062e9d899d20481b1ba52c6d863772
Jason Graffius <jasongraffius@gmail.com>
2017-08-05 20:36:18 -0400

    Added url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject kafunc/kafunc "0.1.2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/kafunc/kafunc"
+  :description "A less-imperative approach to Kafka."
   :dependencies [[org.apache.kafka/kafka_2.11 "0.10.2.1"]
                  [org.clojure/clojure "1.8.0"]]
   :repl-options {:init-ns kafunc.core})
