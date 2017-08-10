960fc042f6ac8d7489da5f9460da9d406f0efa20
Jason Graffius <jasongraffius@gmail.com>
2017-08-09 21:05:01 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafunc/kafunc "0.1.2"
+(defproject kafunc/kafunc "0.1.3-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/kafunc/kafunc"
